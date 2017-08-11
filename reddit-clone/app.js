3025f764c9a912a1a87845446d8eccbf4b91a859
Ryan Barnett <barnettr@kcic.com>
2017-08-11 09:56:17 -0400

    Basic ng directives added
    Bootstrap classes added

--- reddit-clone/app.js
+++ reddit-clone/app.js
@@ -3,17 +3,24 @@ var app = angular.module('existentialNews', []);
 app.controller('MainCtrl', [
   '$scope',
   function($scope){
-    $scope.test = 'Hello World';
     $scope.posts = [
-      {title: 'post 1', upvotes: 3 },
-      {title: 'post 2', upvotes: 1 },
-      {title: 'post 3', upvotes: 23 },
-      {title: 'post 4', upvotes: 8 },
-      {title: 'post 5', upvotes: 4 }
+      {title: 'post 1', link: '', upvotes: 3 },
+      {title: 'post 2', link: '', upvotes: 1 },
+      {title: 'post 3', link: '', upvotes: 23 },
+      {title: 'post 4', link: '', upvotes: 8 },
+      {title: 'post 5', link: '', upvotes: 4 }
     ];
-    $scope.wtfbro = "WTFBRO";
     $scope.addPost = function(){
-      $scope.posts.push({title: 'new post', upvotes: 0 })
+      if(!$scope.title || $scope.title === '') { return; }
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0 });
+      $scope.title = '';
+      $scope.link = '';
     }
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    };
   }
 ])
