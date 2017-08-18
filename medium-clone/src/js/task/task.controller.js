0d826b1eab0de5fe27ba3bb3ba8b4bd869dadd11
Ryan Barnett <barnettr@kcic.com>
2017-08-18 11:24:24 -0400

    Controller saveTask() added and ng-repeat listing task array

--- medium-clone/src/js/task/task.controller.js
+++ medium-clone/src/js/task/task.controller.js
@@ -2,8 +2,14 @@ class TaskCtrl {
   constructor($state){
     'ngInject';
 
+    this.tasks = new Array();
   }
 
+  saveTask(){
+    console.log({"description": this.description});
+    this.tasks.push({"description": this.description});
+    this.description = '';
+  }
 }
 
 export default TaskCtrl;
