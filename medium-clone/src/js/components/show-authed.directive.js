53b92977fdcfc95ae25ee186ee1596d603ba95c3
Ryan Barnett <rmbrntt@gmail.com>
2017-08-20 16:14:57 -0400

    auth directive added for register and login

---
+++ medium-clone/src/js/components/show-authed.directive.js
@@ -0,0 +1,31 @@
+function ShowAuthed(User) {
+  'ngInject';
+
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      scope.User = User;
+
+      scope.$watch('User.current', function(val) {
+        if (val) {
+          if (attrs.showAuthed === 'true') {
+            element.css({display: 'inherit'})
+          }
+          else {
+            element.css({display: 'none'})
+          }
+        }
+        else {
+          if (attrs.showAuthed === 'true') {
+            element.css({display: 'none'})
+          }
+          else {
+            element.css({display: 'inherit'})
+          }
+        }
+      })
+    }
+  }
+}
+
+export default ShowAuthed;
